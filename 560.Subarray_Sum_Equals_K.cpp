990009514bb330920112e04a58d463bd695cc6fb
icoty <icoty.yangy@gmail.com>
2018-12-15 19:47:58 +0800

    560

---
+++ 560.Subarray_Sum_Equals_K.cpp
@@ -0,0 +1,72 @@
+#include "AllInclude.h"
+
+class Solution {
+    public:
+        int subarraySum(vector<int>& nums, int k) {
+            int n = nums.size();
+            if(n < 1)
+                return 0;
+            vector<int> dp(n, 0);
+
+            int ret = 0;
+            for(int i = 0; i < n; ++i)
+            {
+                dp[i] = nums[i];
+                if(dp[i] == k)
+                    ++ret;
+                for(int j = i + 1; j < n; ++j)
+                {
+                    dp[j] = dp[j - 1] + nums[j];
+                    if(dp[j] == k)
+                        ++ret;
+                }
+            }
+            return ret;
+        }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+                return !isspace(ch);
+                }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+                return !isspace(ch);
+                }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int k = stringToInteger(line);
+
+        int ret = Solution().subarraySum(nums, k);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
