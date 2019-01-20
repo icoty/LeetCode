96e33d8aa084062df1e57950b03e7647a3d0e140
icoty <icoty.yangy@gmail.com>
2019-01-20 22:55:44 +0800

    85.Maximal_Rectangle.cpp

---
+++ 85.Maximal_Rectangle.cpp
@@ -0,0 +1,34 @@
+#include "AllInclude.h"
+
+class Solution {
+    public: //https://www.cnblogs.com/grandyang/p/4322667.html
+        int maximalRectangle(vector<vector<char> > &matrix) {
+            int res = 0;
+            vector<int> height;
+            for (int i = 0; i < matrix.size(); ++i) {
+                height.resize(matrix[i].size());
+                for (int j = 0; j < matrix[i].size(); ++j) {
+                    height[j] = matrix[i][j] == '0' ? 0 : (1 + height[j]);
+                    cout << height[j] << " ";
+                }
+                cout << endl;
+                res = max(res, largestRectangleArea(height));
+            }
+            return res;
+        }
+        int largestRectangleArea(vector<int> &height) {
+            int res = 0;
+            stack<int> s;
+            height.push_back(0);
+            for (int i = 0; i < height.size(); ++i) {
+                if (s.empty() || height[s.top()] <= height[i]) s.push(i);
+                else {
+                    int tmp = s.top();
+                    s.pop();
+                    res = max(res, height[tmp] * (s.empty() ? i : (i - s.top() - 1)));
+                    --i;
+                }
+            }
+            return res;
+        }
+};
