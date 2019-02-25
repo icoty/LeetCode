77a00edd0bb539e2630b2f7aa885936751eef63a
icoty <icoty.yangy@gmail.com>
2019-02-25 21:25:54 +0800

    814

---
+++ 921.Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,60 @@
+#include "AllInclude.h"
+
+class Solution {
+    public:
+        int minAddToMakeValid(string S) {
+            stack<char> st;
+            for(auto & ele : S){
+                if(ele == '(')
+                    st.push(ele);
+                else
+                {
+                    if(!st.empty() && st.top() == '(')
+                        st.pop();
+                    else
+                        st.push(ele);
+                }
+            }
+
+            return st.size();
+        }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+            result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string S = stringToString(line);
+
+        int ret = Solution().minAddToMakeValid(S);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
